5b66fa4857d3d646f8b3db2bd34a5049d3d6f8d2
ladh <leexhadrian@gmail.com>
2020-03-17 16:34:35 +0800

    Update output.tf

--- output.tf
+++ output.tf
@@ -3,8 +3,11 @@
 # OUTPUT THE IP OF THE INSTANCE
 # ---------------------------------------------------------------------------------------------------------------------
 
-output "vault_url" {
-  value = "${aws_instance.vault.public_ip}:${var.vault_port}"
+output "vault_public_url" {
+  value = "http://${aws_instance.vault.public_ip}:${var.vault_port}"
 }
 
 
+output "vault_private_url" {
+  value = "http://${aws_instance.vault.private_ip}:${var.vault_port}"
+}
